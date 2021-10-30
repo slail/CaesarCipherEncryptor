e5e3183d4165898bb2fc69d9ee2aa0bbbcf20e43
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-30 08:38:09 -0400

    added encryption algo

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ def caesarCipherEncryptor(string, key):
     A function that returns a new string obtained by shifting every letter in the input string by k positions in the
     alphabet, where k is the key.
     """
-    pass
+    final_string = []
+    newKey = key % 26
+    for letter in string:
+        updateLetter(letter, newKey, final_string)
+    return "".join(final_string)
+
+
+def updateLetter(letter, newKey, final_string):
+    newCode = ord(letter) + newKey
+    if newCode <= ord("z"):
+        final_string.append(chr(newCode))
+    else:
+        final_string.append(chr((ord("a") - 1) + (newCode % ord("z"))))
+
